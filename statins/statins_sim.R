18dbadd86809f4e020daf1b6cac1117ccc3c0581
Michaela Lukacova <dns525@ku.dk>
2026-03-04 10:37:15 +0100

    statin sim

--- statins/statins_sim.R
+++ statins/statins_sim.R
@@ -4,6 +4,7 @@
 
 library(data.table)
 library(simevent)
+library(survival)
 
 #-------------------------------------------------------------------------------
 # Simulating a dummy trial data set
@@ -20,28 +21,125 @@ for(i in 1:22){
 }
 
 # Simulating from simStatinData
-data <- simStatinData(beta = beta, N = 1000, add_cov = add_cov)
-data <- IntFormatData(data, N_cols = 25:30)
+data <- simStatinData(beta = beta, N = 10^6, add_cov = add_cov)
+data <- IntFormatData(data, N_cols = 28:33)
 
 #-------------------------------------------------------------------------------
 # Fitting Models
 #-------------------------------------------------------------------------------
 
+
 # Fit models
-data1 <- subset(data, select = -c(ID, Time, k, N0, N1, N2, N3))
-survfit1 <- coxph(Surv(tstart, tstop, Delta == 0) ~ ., data = data1)
-survfit2 <- coxph(Surv(tstart, tstop, Delta == 1) ~ ., data = data1)
-survfit3 <- coxph(Surv(tstart, tstop, Delta == 2) ~ ., data = data1)
-data4 <- subset(data, select = -c(ID, Time, k, N0, N1, N2, N3))
-survfit4 <- coxph(Surv(tstart, tstop, Delta == 3) ~ . - ID -Time -k -N0 -N1 -N2 -N3, data = data[N3 == 0])
-data5 <- subset(data, select = -c(ID, Time, k, N0, N1, N2, N4))
-survfit5 <- coxph(Surv(tstart, tstop, Delta == 2) ~ . - ID -Time -k -N0 -N1 -N2 -N4, data = data[N4 <= 0])
-data6 <- subset(data, select = -c(ID, Time, k, N0, N1, N2, N5))
-survfit6 <- coxph(Surv(tstart, tstop, Delta == 3) ~ . - ID -Time -k -N0 -N1 -N2 -N5, data = data[N3 <= 0])
-data[which(data$tstart >= data$tstop),]
+# Models where the indidividuals are always at risk
+vars <- setdiff(names(data), c("ID", "Time", "k", "C", "D", "CVD", "Delta", "tstart", "tstop"))
+form <- as.formula(paste("Surv(tstart, tstop, Delta == 0) ~", paste(vars, collapse = " + ")))
+survfit1 <- coxph(form, data = data)
+
+form <- as.formula(paste("Surv(tstart, tstop, Delta == 1) ~", paste(vars, collapse = " + ")))
+survfit2 <- coxph(form, data = data)
+
+form <- as.formula(paste("Surv(tstart, tstop, Delta == 2) ~", paste(vars, collapse = " + ")))
+survfit3 <- coxph(form, data = data)
+
+
+# Models for which the individual is not always at risk
+vars <- setdiff(names(data), c("ID", "Time", "k", "C", "D", "CVD", "OS", "Delta", "tstart", "tstop"))
+form <- as.formula(paste("Surv(tstart, tstop, Delta == 3) ~", paste(vars, collapse = " + ")))
+survfit4 <- coxph(form, data = data[OS == 0])
+
+vars <- setdiff(names(data), c("ID", "Time", "k", "C", "D", "CVD", "A", "Delta", "tstart", "tstop"))
+form <- as.formula(paste("Surv(tstart, tstop, Delta == 4) ~", paste(vars, collapse = " + ")))
+survfit5 <- coxph(form, data = data[A <= 3])
+
+vars <- setdiff(names(data), c("ID", "Time", "k", "C", "D", "CVD", "L", "Delta", "tstart", "tstop"))
+form <- as.formula(paste("Surv(tstart, tstop, Delta == 5) ~", paste(vars, collapse = " + ")))
+survfit6 <- coxph(form, data = data[L <= 3])
+
+# Check estimations
+df <- rbind(rbind(cbind(beta[c(1:24,28:30),3],  confint(survfit3)),
+                  cbind(beta[c(1:24,28:30),2],  confint(survfit2))), 
+            cbind(beta[c(1:24,29:30),4],  confint(survfit4)))
+colnames(df) <- c("actual", "LowCI", "UpCI")
+df <- cbind(df, Index = 1:nrow(df))
+
+ggplot(df) +
+  geom_segment(aes(x = LowCI, xend = UpCI, 
+                   y = Index, yend = Index),
+               colour = "red") +
+  geom_point(aes(x = actual, y = Index))
+
+
+# Der går måske noget galt i process 0...???
 
 #-------------------------------------------------------------------------------
 # Simulating new data, respectively under intervention and without intervention
 #-------------------------------------------------------------------------------
 
+# Der er moget som går galt her....
+
+list_old_vars <- list()
+for(var in 4:27){
+  list_old_vars[(var - 3)] <- data[,..var]
+}
+
+names(list_old_vars) <- colnames(data[, 4:27])
+
+cox_fits <- list(
+  "C" = survfit1,
+  "D" = survfit2,
+  "CVD" = survfit3,
+  "OS" = survfit4,
+  "A" = survfit5,
+  "L" = survfit6
+)
+
+
+sim_data <- simEventCox(
+  10^4,
+  cox_fits,
+  list_old_vars = list_old_vars,
+  n_event_max = c(1, 1, 1, 1, 3, 3),
+  term_events = c(1, 2, 3),
+)
+
+# Sanity check of whether the simulated data corresponds to the original data
+sim_data <- IntFormatData(sim_data, N_cols = 28:33)
+
+# Fit models
+# Models where the indidividuals are always at risk
+vars <- setdiff(names(sim_data), c("ID", "Time", "k", "C", "D", "CVD", "Delta", "tstart", "tstop"))
+form <- as.formula(paste("Surv(tstart, tstop, Delta == 0) ~", paste(vars, collapse = " + ")))
+survfit1 <- coxph(form, data = sim_data)
+
+form <- as.formula(paste("Surv(tstart, tstop, Delta == 1) ~", paste(vars, collapse = " + ")))
+survfit2 <- coxph(form, data = sim_data)
+
+form <- as.formula(paste("Surv(tstart, tstop, Delta == 2) ~", paste(vars, collapse = " + ")))
+survfit3 <- coxph(form, data = sim_data)
+
+# Models for which the individual is not always at risk
+vars <- setdiff(names(data), c("ID", "Time", "k", "C", "D", "CVD", "OS", "Delta", "tstart", "tstop"))
+form <- as.formula(paste("Surv(tstart, tstop, Delta == 3) ~", paste(vars, collapse = " + ")))
+survfit4 <- coxph(form, data = sim_data[OS == 0])
+
+vars <- setdiff(names(data), c("ID", "Time", "k", "C", "D", "CVD", "A", "Delta", "tstart", "tstop"))
+form <- as.formula(paste("Surv(tstart, tstop, Delta == 4) ~", paste(vars, collapse = " + ")))
+survfit5 <- coxph(form, data = sim_data[A < 3])
+
+vars <- setdiff(names(data), c("ID", "Time", "k", "C", "D", "CVD", "L", "Delta", "tstart", "tstop"))
+form <- as.formula(paste("Surv(tstart, tstop, Delta == 5) ~", paste(vars, collapse = " + ")))
+survfit6 <- coxph(form, data = sim_data[L < 3])
+
+# Check estimations
+df <- rbind(rbind(cbind(beta[c(1:24,28:30),1],  confint(survfit3)),
+                  cbind(beta[c(1:24,28:30),2],  confint(survfit2))), 
+            cbind(beta[c(1:24,29:30),4],  confint(survfit4)))
+colnames(df) <- c("actual", "LowCI", "UpCI")
+df <- cbind(df, Index = 1:nrow(df))
+
+ggplot(df) +
+  geom_segment(aes(x = LowCI, xend = UpCI, 
+                   y = Index, yend = Index),
+               colour = "red") +
+  geom_point(aes(x = actual, y = Index))
 
