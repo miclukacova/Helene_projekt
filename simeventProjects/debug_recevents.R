2e96c06b3360066b5ceed32c4b9c61325891f332
Michaela Lukacova <dns525@ku.dk>
2026-06-12 08:37:37 +0200

    cleaning up

--- simeventProjects/debug_recevents.R
+++ simeventProjects/debug_recevents.R
@@ -164,20 +164,15 @@ data_obs[, summary(Time)]
 # DET HER VIRKER RET GODT! PRØV DET SAMME MED SIMEVENTOBJ
 ################################################################################
 
-N <- 10000
+N <- 10^4
+#-------------------------------------------------------------------------------
+# Competing risk
+#-------------------------------------------------------------------------------
 
-# In simDisease
-
-# The observed data
-data_obs <- simDisease(N)
-# T0 -> T1
-data1 <- data_obs[order(Time), .SD[1], by = ID]
-setkey(data1, ID)
-
-# Fit some Cox models
-cox_cens <- survival::coxph(survival::Surv(Time, Delta == 0) ~ L0 + A0, data = data1)
-cox_death <- survival::coxph(survival::Surv(Time, Delta == 1) ~ L0 + A0, data = data1)
-cox_Disease <- survival::coxph(survival::Surv(Time, Delta == 2) ~ L0 + A0, data = data1)
+data_obs <- simCRdata(N)
+cox_cens <- survival::coxph(survival::Surv(Time, Delta == 0) ~ L0 + A0, data = data_obs)
+cox_death <- survival::coxph(survival::Surv(Time, Delta == 1) ~ L0 + A0, data = data_obs)
+cox_Disease <- survival::coxph(survival::Surv(Time, Delta == 2) ~ L0 + A0, data = data_obs)
 
 # Equip with predict2 method
 predict2 <- function(obj, ...) {
@@ -195,14 +190,62 @@ predict2.simevent <- function(obj, sim_data){
   # Chf
   chf_list <- lapply(1:length(obj), function(j) cox_term[[j]] %*% t(basehazz_list[[j]][["hazard"]]))
   chf <- array(dim = c(c(dim(chf_list[[1]])), length(obj)))
-  for(j in 1:length(obj)) {
-    chf[,,j] <- chf_list[[j]]
-    chf[,,j] <- cbind(matrix(data = 0, ncol = 1, nrow = N), chf[,,j])
-  }
+  for(j in 1:length(obj)) chf[,,j] <- chf_list[[j]]
   
   # Return list
   list(time = basehazz_list[[1]][["time"]], chf = chf)
 }
+
+# Then simulate new data:
+cox_fits <- list("C" = cox_cens, "D" = cox_death, "L" = cox_Disease)
+class(cox_fits) <- "simevent"
+list_old_vars <- list("L0" = data_obs$L0, "A0" = data_obs$A0)
+new_data <- simEventObj(N, obj = cox_fits, list_old_vars = list_old_vars, event_names = c("C", "D", "L"))
+
+# Fit new Cox models
+cox_cens2 <- survival::coxph(survival::Surv(Time, Delta == 1) ~ L0 + A0, data = new_data)
+cox_death2 <- survival::coxph(survival::Surv(Time, Delta == 2) ~ L0 + A0, data = new_data)
+cox_Disease2 <- survival::coxph(survival::Surv(Time, Delta == 3) ~ L0 + A0, data = new_data)
+
+# Compute confidence intervals
+ci_cens <- confint(cox_cens2)
+coef_cens <- cox_cens$coefficients
+
+ci_death <- confint(cox_death2)
+coef_death <- cox_death$coefficients
+
+ci_Disease <- confint(cox_Disease2)
+coef_Disease <- cox_Disease$coefficients
+
+# Compare confidence intervals and true values
+all(coef_cens >= ci_cens[, 1] & coef_cens <= ci_cens[, 2])
+all(coef_death >= ci_death[, 1] & coef_death <= ci_death[, 2])
+all(coef_Disease >= ci_Disease[, 1] & coef_Disease <= ci_Disease[, 2])
+
+
+ggarrange(plotEventData(new_data[1:2000,], title = "Sim Data"),
+          plotEventData(data_obs[1:2000,], title = "Original Data"), ncol = 2)
+
+new_data[, table(Delta)]/nrow(new_data)
+data_obs[, table(Delta)]/nrow(data_obs)
+new_data[, summary(Time)]
+data_obs[, summary(Time)]
+
+
+#-------------------------------------------------------------------------------
+# Disease setting
+#-------------------------------------------------------------------------------
+
+# The observed data
+data_obs <- simDisease(N)
+# T0 -> T1
+data1 <- data_obs[order(Time), .SD[1], by = ID]
+setkey(data1, ID)
+
+# Fit some Cox models
+cox_cens <- survival::coxph(survival::Surv(Time, Delta == 0) ~ L0 + A0, data = data1)
+cox_death <- survival::coxph(survival::Surv(Time, Delta == 1) ~ L0 + A0, data = data1)
+cox_Disease <- survival::coxph(survival::Surv(Time, Delta == 2) ~ L0 + A0, data = data1)
 
 # Then simulate new data:
 cox_fits <- list("C" = cox_cens, "D" = cox_death, "L" = cox_Disease)
@@ -238,4 +281,54 @@ new_data[, table(Delta)]/nrow(new_data)
 data1[, table(Delta)]/nrow(data1)
 new_data[, summary(Time)]
 data1[, summary(Time)]
+
+# T1 -> T2
+data2 <- data_obs[ID %in% data1[!(Delta %in% c(0,1)), ID], ]
+data2[, oldTime := min(Time), by = ID]
+data2[, newTime := Time - oldTime, by = ID]
+data2 <- data2[order(Time), .SD[2], by = ID]
+setkey(data2, ID)
+
+# Fit some Cox models
+cox_cens <- survival::coxph(survival::Surv(Time, Delta == 0) ~ L0 + A0, data = data2)
+cox_death <- survival::coxph(survival::Surv(Time, Delta == 1) ~ L0 + A0, data = data2)
+
+# Then simulate new data:
+cox_fits <- list("C" = cox_cens, "D" = cox_death)
+class(cox_fits) <- "simevent"
+list_old_vars <- list("L0" = data_obs$L0, "A0" = data_obs$A0)
+new_data <- simEventObj(N, obj = cox_fits, list_old_vars = list_old_vars, event_names = c("C", "D"))
+
+# Fit new Cox models
+cox_cens2 <- survival::coxph(survival::Surv(Time, Delta == 1) ~ L0 + A0, data = new_data)
+cox_death2 <- survival::coxph(survival::Surv(Time, Delta == 2) ~ L0 + A0, data = new_data)
+
+# Compute confidence intervals
+ci_cens <- confint(cox_cens2)
+coef_cens <- cox_cens$coefficients
+
+ci_death <- confint(cox_death2)
+coef_death <- cox_death$coefficients
+
+ci_Disease <- confint(cox_Disease2)
+coef_Disease <- cox_Disease$coefficients
+
+# Compare confidence intervals and true values
+all(coef_cens >= ci_cens[, 1] & coef_cens <= ci_cens[, 2])
+all(coef_death >= ci_death[, 1] & coef_death <= ci_death[, 2])
+
+
+ggarrange(plotEventData(new_data[1:2000,], title = "Sim Data"),
+          plotEventData(data2[1:2000,], title = "Original Data"), ncol = 2)
+
+new_data[, table(Delta)]/nrow(new_data)
+data2[, table(Delta)]/nrow(data2)
+new_data[, summary(Time)]
+data2[, summary(Time)]
+
+# Jeg har det som om at det her virker ? Der er noge tmed at vi ikke får præcis de samme covariater som dem vi simulere fra
+
+
+
+
 
